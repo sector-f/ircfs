ae3fb12dcdc6302585a5c4315516e5a8b56d4da1
Adam <sector-f@users.noreply.github.com>
2017-05-01 22:51:35 -0400

    Added config toml parsing

---
+++ examples/config_test.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate toml;
+
+extern crate riiir;
+use riiir::config;
+
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    let mut file = File::open("examples/riiir.toml").unwrap();
+    let mut buf = String::new();
+    let _ = file.read_to_string(&mut buf);
+
+    let deserialized: config::Config = toml::from_str(&buf).unwrap();
+
+    println!("{:#?}", deserialized);
+}
---
+++ src/config.rs
@@ -0,0 +1,22 @@
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    global: GlobalConfig,
+    #[serde(rename = "server")]
+    servers: Vec<ServerConfig>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GlobalConfig {
+    username: String,
+    realname: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ServerConfig {
+    username: Option<String>,
+    realname: Option<String>,
+    address: String,
+    port: Option<u16>,
+    alias: Option<String>,
+    ssl: Option<bool>,
+}
