563cdb8ace368d5d91c92e86ceb56bb72c1a0194
Adam <sector-f@users.noreply.github.com>
2017-05-19 02:49:45 -0400

    Refactored again (and broke it again)

--- src/ircfs.rs
+++ src/ircfs.rs
@@ -15,24 +15,54 @@ use std::ffi::{OsStr, OsString};
 use std::collections::HashMap;
 use std::os::raw::c_int;
 
+#[derive(Debug)]
 pub struct IrcFs {
-    files: HashMap<u64, IrcDir>, // Maps dir inodes to servers
-    dir_map: HashMap<u64, u64>, // Maps in/out inodes to dir inodes
+    files: HashMap<u64, FuseFile>,
     attr: FileAttr, // Attributes for filesystem root dir
     highest_inode: u64,
 }
 
+#[derive(Debug)]
+enum FuseFile {
+    Dir(IrcDir),
+    File(IrcFile),
+}
+
+#[derive(Debug)]
 struct IrcDir {
     name: OsString,
     // server: IrcServer,
+    parent: u64, // Probably always gonna be 1
     attr: FileAttr,
-    infile: IrcFile,
-    outfile: IrcFile,
+    in_inode: u64,
+    out_inode: u64,
+}
+
+#[derive(Debug)]
+pub struct IrcFile {
+    parent: u64,
+    attr: FileAttr,
     buf: Vec<u8>,
 }
 
+impl FuseFile {
+    pub fn parent(&self) -> u64 {
+        match *self {
+            FuseFile::Dir(ref dir) => dir.parent,
+            FuseFile::File(ref file) => file.parent,
+        }
+    }
+
+    pub fn attr(&self) -> FileAttr {
+        match *self {
+            FuseFile::Dir(ref dir) => dir.attr,
+            FuseFile::File(ref file) => file.attr,
+        }
+    }
+}
+
 impl IrcDir {
-    fn new(name: OsString, ino: u64, infile: IrcFile, outfile: IrcFile) -> Self {
+    fn new(name: OsString, ino: u64, in_inode: u64, out_inode: u64) -> Self {
         let init_time = time::get_time();
 
         let attr = FileAttr {
@@ -54,26 +84,22 @@ impl IrcDir {
 
         IrcDir {
             name: name,
+            parent: 1, // Hard-code this (for now?)
             attr: attr,
-            infile: infile,
-            outfile: outfile,
-            buf: Vec::new(),
+            in_inode: in_inode,
+            out_inode: out_inode,
         }
     }
 }
 
-pub struct IrcFile {
-    attr: FileAttr,
-}
-
 impl IrcFile {
-    fn new(ino: u64,) -> Self {
+    fn new(ino: u64, parent: u64) -> Self {
         let init_time = time::get_time();
 
         let attr = FileAttr {
             ino: ino,
             size: 0,
-            blocks: 0,
+            blocks: 1,
             atime: init_time,
             mtime: init_time,
             ctime: init_time,
@@ -87,7 +113,16 @@ impl IrcFile {
             flags: 0,
         };
 
-        IrcFile { attr: attr }
+        IrcFile {
+            parent: parent,
+            attr: attr,
+            buf: Vec::new(),
+        }
+    }
+
+    fn insert_data(&mut self, data: &[u8]) {
+        self.buf.extend_from_slice(data);
+        self.attr.size += data.len() as u64;
     }
 }
 
@@ -114,27 +149,32 @@ impl IrcFs {
 
         IrcFs {
             files: HashMap::new(),
-            dir_map: HashMap::new(),
             attr: attr,
             highest_inode: 1,
         }
     }
 
     pub fn add_server(&mut self, name: OsString) {
-        let ino = self.highest_inode;
-        let infile = IrcFile::new(ino+2);
-        let outfile = IrcFile::new(ino+3);
-        let dir = IrcDir::new(name, ino+1, infile, outfile);
-        self.files.insert(ino+1, dir);
-        self.dir_map.insert(ino+2, ino+1);
-        self.dir_map.insert(ino+3, ino+1);
+        let dir_ino = self.highest_inode + 1;
+        let in_ino = dir_ino + 1;
+        let out_ino = dir_ino + 2;
+
+        let infile = IrcFile::new(in_ino, dir_ino);
+        let mut outfile = IrcFile::new(out_ino, dir_ino);
+        outfile.insert_data("Test data\n".as_bytes());
+        let dir = IrcDir::new(name, dir_ino, in_ino, out_ino);
+
+        self.files.insert(dir_ino, FuseFile::Dir(dir));
+        self.files.insert(in_ino, FuseFile::File(infile));
+        self.files.insert(out_ino, FuseFile::File(outfile));
+
         self.highest_inode += 3;
         self.attr.nlink += 1;
     }
 }
 
 impl Filesystem for IrcFs {
-    fn init(&mut self, _req: &Request) -> Result<(), c_int> {
+    fn init(&mut self, req: &Request) -> Result<(), c_int> {
         let foo = OsString::from("foo");
         let bar = OsString::from("bar");
         let baz = OsString::from("baz");
@@ -142,6 +182,9 @@ impl Filesystem for IrcFs {
         self.add_server(foo);
         self.add_server(bar);
         self.add_server(baz);
+
+        self.attr.uid = req.uid();
+        self.attr.gid = req.gid();
 
         // let config = Config {
         //     nickname: Some("riiir-nickname".to_string()),
@@ -173,23 +216,14 @@ impl Filesystem for IrcFs {
     }
 
     fn getattr(&mut self, _req: &Request, req_ino: u64, reply: ReplyAttr) {
-        println!("getattr(ino={})", req_ino);
         let ttl = Timespec::new(1, 0);
         if req_ino == 1 {
             reply.attr(&ttl, &self.attr);
             return;
         } else {
-            for (dir_ino, dir) in &self.files {
-                if &req_ino == dir_ino {
-                    reply.attr(&ttl, &dir.attr);
-                    return;
-                } else if req_ino == dir.infile.attr.ino {
-                    reply.attr(&ttl, &dir.infile.attr);
-                    return;
-                } else if req_ino == dir.outfile.attr.ino {
-                    reply.attr(&ttl, &dir.outfile.attr);
-                    return;
-                }
+            if let Some(file) = self.files.get(&req_ino) {
+                reply.attr(&ttl, &file.attr());
+                return;
             }
         }
         reply.error(ENOENT);
@@ -198,26 +232,32 @@ impl Filesystem for IrcFs {
     fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
         let ttl = Timespec::new(1, 0);
         if parent == 1 {
-            for (inode, dir) in &self.files {
-                if name == dir.name {
-                    reply.entry(&ttl, &self.files[&inode].attr, 0);
-                    return;
+            for (inode, file) in &self.files {
+                match *file {
+                    FuseFile::Dir(ref dir) => {
+                        if name == dir.name {
+                            reply.entry(&ttl, &dir.attr, 0);
+                            return;
+                        }
+                    },
+                    _ => {},
                 }
             }
         } else {
-            for (inode, dir) in &self.files {
-                if &parent == inode {
-                    if name == "in" {
-                        reply.entry(&ttl, &dir.infile.attr, 0);
-                        return;
-                    } else if name == "out" {
-                        reply.entry(&ttl, &dir.outfile.attr, 0);
-                        return;
-                    } else {
-                        reply.error(ENOENT);
-                        return;
+            for (inode, file) in &self.files {
+                if &parent == &file.parent() {
+                    match *file {
+                        FuseFile::File(ref file) => {
+                            if name == "in" || name == "out" {
+                                reply.entry(&ttl, &file.attr, 0);
+                                return;
+                            } else {
+                                reply.error(ENOENT);
+                                return;
+                            }
+                        },
+                        _ => {},
                     }
-
                 }
             }
         }
@@ -229,37 +269,47 @@ impl Filesystem for IrcFs {
             if ino == 1 {
                 reply.add(1, 0, FileType::Directory, ".");
                 reply.add(1, 1, FileType::Directory, "..");
-                for (inode, server) in &self.files {
-                    reply.add(inode.clone(), inode.clone(), FileType::Directory, &server.name);
+                for (inode, file) in &self.files {
+                    match *file {
+                        FuseFile::Dir(ref dir) => {
+                            let ino = dir.attr.ino;
+                            reply.add(ino, ino, FileType::Directory, &dir.name);
+                        },
+                        _ => {},
+                    }
                 }
                 reply.ok();
                 return;
             } else {
-                match self.files.get(&ino) {
-                    Some(dir) => {
-                        reply.add(1, 0, FileType::Directory, ".");
-                        reply.add(1, 1, FileType::Directory, "..");
+                if let Some(file) = self.files.get(&ino) {
+                    match *file {
+                        FuseFile::Dir(ref dir) => {
+                            reply.add(1, 0, FileType::Directory, ".");
+                            reply.add(1, 1, FileType::Directory, "..");
 
-                        reply.add(
-                            dir.infile.attr.ino,
-                            dir.infile.attr.ino,
-                            FileType::RegularFile,
-                            "in",
-                        );
-                        reply.add(
-                            dir.outfile.attr.ino,
-                            dir.outfile.attr.ino,
-                            FileType::RegularFile,
-                            "out",
-                        );
+                            let in_inode = dir.in_inode;
+                            let out_inode = dir.out_inode;
 
-                        reply.ok();
-                    },
-                    None => {
-                        reply.error(ENOENT);
-                    },
+                            reply.add(
+                                in_inode,
+                                in_inode,
+                                FileType::RegularFile,
+                                "in",
+                            );
+
+                            reply.add(
+                                out_inode,
+                                out_inode,
+                                FileType::RegularFile,
+                                "out",
+                            );
+
+                            reply.ok();
+                            return;
+                        },
+                        _ => {},
+                    }
                 }
-                return;
             }
         }
         reply.error(ENOENT);
@@ -272,10 +322,14 @@ impl Filesystem for IrcFs {
         offset: u64,
         size: u32,
         reply: ReplyData) {
-        println!("read(ino={}, fh={}, offset={}, size={})", req_ino, fh, offset, size);
 
-        if let Some(_dir) = self.dir_map.get(&req_ino) {
-            reply.data(&"Hello, world!".as_bytes()[offset as usize..]);
+        if let Some(file) = self.files.get(&req_ino) {
+            match *file {
+                FuseFile::File(ref file) => {
+                    reply.data(&file.buf[offset as usize..]);
+                },
+                _ => {},
+            }
         } else {
             reply.error(ENOENT);
         }
