32e678aa659be0ce101502b6983952e84c28ba6f
Adam <sector-f@users.noreply.github.com>
2017-06-08 01:52:58 -0400

    src/ is still a mess, but progress is being made

--- src/mem_safe_fs.rs
+++ src/mem_safe_fs.rs
@@ -1,280 +1,80 @@
-use fuse_mt::{FileAttr, FileType};
+use fuse_mt::{FileAttr, FileType, RequestInfo};
 
 extern crate time;
 
 use std::collections::HashMap;
-use std::ffi::OsString;
+use std::ffi::{OsString, OsStr};
 use std::path::{PathBuf, Path};
-use std::sync::{Arc, RwLock, Mutex, RwLockReadGuard};
 use std::io::{self, Error, ErrorKind};
 
-////////////////
-// Filesystem //
-////////////////
-
-fn new_lock<T>(item: T) -> Arc<RwLock<T>> {
-    Arc::new(RwLock::new(item))
-}
-
 pub struct Filesystem {
-    dir_tree: Arc<RwLock<LookupDir>>,
-    files: Arc<RwLock<HashMap<usize, Arc<RwLock<Node>>>>>,
-    highest_fh: Arc<RwLock<usize>>,
+    fake_root: FuseDir,
 }
 
 impl Filesystem {
     pub fn new(uid: u32, gid: u32) -> Self {
-        let mut root = LookupDir::new(0);
-        root.mk_dir("/", 1);
-
-        let mut files = HashMap::new();
-        files.insert(1, new_lock(FuseDir::new(uid, gid).into()));
+        let mut root = FuseDir::new(uid, gid);
+        root.mk_dir("/", uid, gid).unwrap();
 
         Filesystem {
-            dir_tree: new_lock(root),
-            files: new_lock(files),
-            highest_fh: new_lock(1),
+            fake_root: root,
         }
     }
 
-    pub fn get<P: AsRef<Path>>(&self, path: P) -> Option<&Arc<RwLock<Node>>> {
-        let path = path.as_ref();
-
-        if path.is_relative() {
-            return None;
-        }
-
-        let dir_tree = &*self.dir_tree.read().unwrap();
-
-        let fh = match dir_tree.get(path.strip_prefix("/").unwrap()) {
-            Some(node) => node.fh(),
-            None => return None,
-        };
-
-        let files = self.files.clone();
-        files.read().unwrap().get(&fh)
-
-        // self.files.read().unwrap().get(&fh).clone()
+    pub fn get<P: AsRef<Path>>(&self, path: P, _req: &RequestInfo) -> io::Result<&Node> {
+        self.fake_root.get(path)
     }
 
-    pub fn mk_dir<P: AsRef<Path>>(&self, path: P, fh: usize) -> io::Result<()> {
-        // let path = path.as_ref();
+    pub fn get_mut<P: AsRef<Path>>(&mut self, path: P, _req: &RequestInfo) -> io::Result<&mut Node> {
+        self.fake_root.get_mut(path)
+    }
 
-        // if path.is_relative() {
-        //     return Err(Error::new(ErrorKind::NotFound, "Given path must be absolute"));
+    pub fn mk_dir<P: AsRef<Path>>(&mut self, path: P, _req: &RequestInfo) -> io::Result<()> {
+        let uid = self.root_dir().attr.uid;
+        let gid = self.root_dir().attr.gid;
+        self.fake_root.mk_dir(path, uid, gid)
+    }
+
+    pub fn mk_ro_file<P: AsRef<Path>>(&mut self, path: P, _req: &RequestInfo) -> io::Result<()> {
+        let uid = self.root_dir().attr.uid;
+        let gid = self.root_dir().attr.gid;
+        self.fake_root.mk_ro_file(path, uid, gid)
+    }
+
+    pub fn mk_rw_file<P: AsRef<Path>>(&mut self, path: P, _req: &RequestInfo) -> io::Result<()> {
+        let uid = self.root_dir().attr.uid;
+        let gid = self.root_dir().attr.gid;
+        self.fake_root.mk_rw_file(path, uid, gid)
+    }
+
+    pub fn dir_list<P: AsRef<Path>>(&self, path: P, _req: &RequestInfo) -> Option<&[&OsStr]> {
+        // if let Some(&Node::D(ref dir)) = self.get(path) {
+            unimplemented!();
+        // } else {
+            // None
         // }
-
-        // let dir_tree = self.dir_tree.clone();
-
-        // match dir_tree.write().unwrap().mk_dir(path, fh) {
-        //     Ok(_) => {
-        //         *self.highest_fh.write().unwrap() += 1;
-
-        //         let uid = unimplemented!();
-        //         let gid = unimplemented!();
-
-        //         let fh = self.highest_fh.clone();
-
-        //         self.files.insert(*fh.read().unwrap(), Arc::new(RwLock::new(FuseDir::new(uid, gid).into())));
-
-        //         return Ok(());
-        //     },
-        //     Err(e) => {
-        //         return Err(e);
-        //     },
-        // }
-
-        unreachable!();
     }
 
-    pub fn mk_ro_file<P: AsRef<Path>>(&self, path: P, fh: usize) -> io::Result<()> {
-        unimplemented!();
+    fn root_dir(&self) -> &FuseDir {
+        self.fake_root.get("/").unwrap().as_dir()
     }
 
-    pub fn mk_rw_file<P: AsRef<Path>>(&self, path: P, fh: usize) -> io::Result<()> {
-        unimplemented!();
+    fn root_dir_mut(&mut self) -> &mut FuseDir {
+        self.fake_root.get_mut("/").unwrap().as_mut_dir()
     }
 }
-
-////////////
-// Lookup //
-////////////
-
-pub struct LookupDir {
-    tree: HashMap<OsString, LookupNode>,
-    fh: usize,
-}
-
-impl LookupDir {
-    fn new(fh: usize) -> Self {
-        LookupDir {
-            tree: HashMap::new(),
-            fh: fh,
-        }
-    }
-
-    fn get<P: AsRef<Path>>(&self, path: P) -> Option<&LookupNode> {
-        let path = path.as_ref();
-
-        let mut iter = path.iter();
-        let first_segment = match iter.next() {
-            Some(segment) => segment,
-            None => return None,
-        };
-
-        let mut node = match self.tree.get(first_segment) {
-            Some(node) => node,
-            None => return None,
-        };
-
-        for segment in iter {
-            match *node {
-                LookupNode::F(ref _file) => return None,
-                LookupNode::D(ref dir) => node = match dir.tree.get(segment) {
-                    Some(node) => node,
-                    None => return None,
-                }
-            }
-        }
-
-        Some(node)
-    }
-
-    fn get_mut<P: AsRef<Path>>(&mut self, path: P) -> Option<&mut LookupNode> {
-        let path = path.as_ref();
-
-        let mut iter = path.iter();
-        let first_segment = match iter.next() {
-            Some(segment) => segment,
-            None => return None,
-        };
-
-        let mut node = match self.tree.get_mut(first_segment) {
-            Some(node) => node,
-            None => return None,
-        };
-
-        for segment in iter {
-            match *{node} {
-                LookupNode::F(ref mut _file) => return None,
-                LookupNode::D(ref mut dir) => node = match dir.tree.get_mut(segment) {
-                    Some(node) => node,
-                    None => return None,
-                }
-            }
-        }
-
-        Some(node)
-    }
-
-    fn mk_dir<P: AsRef<Path>>(&mut self, path: P, fh: usize) -> io::Result<()> {
-        self.insert_node(path, LookupDir::new(fh).into())
-    }
-
-    fn mk_file<P: AsRef<Path>>(&mut self, path: P, fh: usize) -> io::Result<()> {
-        self.insert_node(path, LookupFile::new(fh).into())
-    }
-
-    fn insert_node<P: AsRef<Path>>(&mut self, path: P, node: LookupNode) -> io::Result<()> {
-        let path = path.as_ref();
-
-        let parent = path.parent();
-        let filename = path.file_name()
-            .ok_or(Error::new(ErrorKind::InvalidInput, "No filename specified"))?;
-
-        if parent == Some(&Path::new("")) {
-            if let Some(_n) = self.tree.get_mut(filename) {
-                return Err(Error::new(ErrorKind::AlreadyExists, "File already exists"));
-            }
-            self.tree.insert(filename.to_owned(), node);
-            Ok(())
-        } else {
-            if let Some(segment) = parent {
-                match self.get_mut(segment) {
-                    Some(&mut LookupNode::D(ref mut dir)) => {
-                        if let None = dir.get(Path::new(filename)) {
-                            dir.tree.insert(filename.to_owned(), node);
-                            Ok(())
-                        } else {
-                            Err(Error::new(ErrorKind::AlreadyExists, "File already exists"))
-                        }
-                    },
-                    Some(&mut LookupNode::F(ref mut _file)) => {
-                        Err(Error::new(ErrorKind::Other, "Not a directory"))
-                    },
-                    None => {
-                        Err(Error::new(ErrorKind::NotFound, "No such file or directory"))
-                    },
-                }
-            } else {
-                Err(Error::new(ErrorKind::NotFound, "No such file or directory"))
-            }
-        }
-    }
-}
-
-pub struct LookupFile {
-    fh: usize,
-}
-
-impl LookupFile {
-    fn new(fh: usize) -> Self {
-        LookupFile {
-            fh: fh,
-        }
-    }
-}
-
-pub enum LookupNode {
-    F(LookupFile),
-    D(LookupDir),
-}
-
-impl LookupNode {
-    fn fh(&self) -> usize {
-        match *self {
-            LookupNode::F(ref file) => file.fh,
-            LookupNode::D(ref dir) => dir.fh,
-        }
-    }
-}
-
-impl From<LookupFile> for LookupNode {
-    fn from(f: LookupFile) -> LookupNode {
-        LookupNode::F(f)
-    }
-}
-
-impl From<LookupDir> for LookupNode {
-    fn from(d: LookupDir) -> LookupNode {
-        LookupNode::D(d)
-    }
-}
-
-//////////////////
-// Actual files //
-//////////////////
 
 pub struct FuseDir {
+    tree: HashMap<OsString, Node>,
     attr: FileAttr,
-}
-
-pub struct FuseFile {
-    attr: FileAttr,
-    uw: bool, // user-writable
-    data: Vec<u8>,
-}
-
-pub enum Node {
-    F(FuseFile),
-    D(FuseDir),
 }
 
 impl FuseDir {
-    pub fn new(uid: u32, gid: u32) -> Self {
+    fn new(uid: u32, gid: u32) -> Self {
         let init_time = time::get_time();
 
         FuseDir {
+            tree: HashMap::new(),
             attr: FileAttr {
                 size: 4096,
                 blocks: 8,
@@ -292,6 +92,124 @@ impl FuseDir {
             }
         }
     }
+
+    fn get<P: AsRef<Path>>(&self, path: P) -> io::Result<&Node> {
+        let path = path.as_ref();
+
+        let mut iter = path.iter();
+        let first_segment = match iter.next() {
+            Some(segment) => segment,
+            None => return Err(Error::from(ErrorKind::NotFound)),
+        };
+
+        let mut node = match self.tree.get(first_segment) {
+            Some(node) => node,
+            None => return Err(Error::from(ErrorKind::NotFound)), // This is erroring
+        };
+
+        for segment in iter {
+            match *node {
+                Node::F(ref _file) => return Err(Error::from(ErrorKind::NotFound)),
+                Node::D(ref dir) => node = match dir.tree.get(segment) {
+                    Some(node) => node,
+                    None => return Err(Error::from(ErrorKind::NotFound)),
+                }
+            }
+        }
+
+        Ok(node)
+    }
+
+    fn get_mut<P: AsRef<Path>>(&mut self, path: P) -> io::Result<&mut Node> {
+        let path = path.as_ref();
+
+        let mut iter = path.iter();
+        let first_segment = match iter.next() {
+            Some(segment) => segment,
+            None => return Err(Error::from(ErrorKind::NotFound)),
+        };
+
+        let mut node = match self.tree.get_mut(first_segment) {
+            Some(node) => node,
+            None => return Err(Error::from(ErrorKind::NotFound)),
+        };
+
+        for segment in iter {
+            match *{node} {
+                Node::F(ref mut _file) => return Err(Error::from(ErrorKind::NotFound)),
+                Node::D(ref mut dir) => node = match dir.tree.get_mut(segment) {
+                    Some(node) => node,
+                    None => return Err(Error::from(ErrorKind::NotFound)),
+                }
+            }
+        }
+
+        Ok(node)
+    }
+
+    fn mk_dir<P: AsRef<Path>>(&mut self, path: P, uid: u32, gid: u32) -> io::Result<()> {
+        self.insert_node(path, FuseDir::new(uid, gid).into())
+    }
+
+    fn mk_rw_file<P: AsRef<Path>>(&mut self, path: P, uid: u32, gid: u32) -> io::Result<()> {
+        self.insert_node(path, FuseFile::new(uid, gid, true).into())
+    }
+
+    fn mk_ro_file<P: AsRef<Path>>(&mut self, path: P, uid: u32, gid: u32) -> io::Result<()> {
+        self.insert_node(path, FuseFile::new(uid, gid, false).into())
+    }
+
+    fn insert_node<P: AsRef<Path>>(&mut self, path: P, node: Node) -> io::Result<()> {
+        let path = path.as_ref();
+
+        // Needed for making the root
+        if path == Path::new("/") {
+            if let Some(_node) = self.tree.get(OsStr::new(path)) {
+                return Err(Error::from(ErrorKind::AlreadyExists));
+            }
+            self.tree.insert(OsString::from("/"), node);
+            Ok(())
+        } else {
+            let parent = path.parent();
+            let filename = path.file_name()
+                .ok_or(Error::from(ErrorKind::InvalidInput,))?;
+
+            if parent == Some(&Path::new("")) {
+                if let Some(_n) = self.tree.get_mut(filename) {
+                    return Err(Error::new(ErrorKind::AlreadyExists, "File already exists"));
+                }
+                self.tree.insert(filename.to_owned(), node);
+                Ok(())
+            } else {
+                if let Some(segment) = parent {
+                    match self.get_mut(segment) {
+                        Ok(&mut Node::D(ref mut dir)) => {
+                            if let Err(_e) = dir.get(Path::new(filename)) {
+                                dir.tree.insert(filename.to_owned(), node);
+                                Ok(())
+                            } else {
+                                Err(Error::from(ErrorKind::AlreadyExists))
+                            }
+                        },
+                        Ok(&mut Node::F(ref mut _file)) => {
+                            Err(Error::from(ErrorKind::Other))
+                        },
+                        Err(_e) => {
+                            Err(Error::from(ErrorKind::NotFound))
+                        },
+                    }
+                } else {
+                    Err(Error::from(ErrorKind::NotFound))
+                }
+            }
+        }
+    }
+}
+
+pub struct FuseFile {
+    attr: FileAttr,
+    uw: bool,
+    data: Vec<u8>,
 }
 
 impl FuseFile {
@@ -323,6 +241,35 @@ impl FuseFile {
 
     pub fn insert_data(&mut self, data: &[u8]) {
         self.data.extend_from_slice(data);
+    }
+}
+
+pub enum Node {
+    F(FuseFile),
+    D(FuseDir),
+}
+
+
+impl Node {
+    fn as_dir(&self) -> &FuseDir {
+        match self {
+            &Node::D(ref dir) => dir,
+            &Node::F(ref _file) => panic!(),
+        }
+    }
+
+    fn as_mut_dir(&mut self) -> &mut FuseDir {
+        match self {
+            &mut Node::D(ref mut dir) => dir,
+            &mut Node::F(ref mut _file) => panic!(),
+        }
+    }
+
+    fn attr(&self) -> &FileAttr {
+        match *self {
+            Node::F(ref file) => &file.attr,
+            Node::D(ref dir) => &dir.attr,
+        }
     }
 }
 
--- src/mem_safe_ircfs.rs
+++ src/mem_safe_ircfs.rs
@@ -2,32 +2,30 @@ use fuse_mt::*;
 use mem_safe_fs::*;
 // use tree_fs::*;
 use config::FsConfig;
-use std::sync::RwLock;
 use libc::c_int;
 use std::ops::Deref;
+use std::sync::{Arc, RwLock};
 
 pub struct IrcFs {
-    root: Filesystem,
+    fs: Arc<RwLock<Filesystem>>,
 }
 
 impl IrcFs {
     pub fn new(config: &FsConfig, uid: u32, gid: u32) -> Self {
-        unimplemented!();
+        IrcFs {
+            fs: Arc::new(RwLock::new(Filesystem::new(uid, gid))),
+        }
     }
 }
 
 impl FilesystemMT for IrcFs {
     fn init(&self, req: RequestInfo) -> ResultEmpty {
-        // let lock = self.root.get("/in").unwrap();
-        // let ref mut in_file = *lock.write().unwrap();
+        let mut fs = self.fs.write().unwrap();
 
-        // match *in_file {
-        //     Node::F(ref mut file) => {
-        //         file.insert_data("Testing\n".as_bytes());
-        //     },
-        //     Node::D(ref mut dir) => {
-        //     },
-        // }
+        fs.get("/", &req).unwrap();
+
+        // fs.mk_rw_file("/in", &req).unwrap();
+        // fs.mk_ro_file("/out", &req).unwrap();
 
         Ok(())
     }
